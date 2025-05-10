5fb54572ed9788bb1361c0b927f959605ec9ccae
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-10 19:26:30 +0200

    finished the game over scene

--- src/uSceneGameOver.pas
+++ src/uSceneGameOver.pas
@@ -1,6 +1,40 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Bidioo
+///
+/// Copyright 2013-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://bidioo.gamolf.fr
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Bidioo-v2-Delphi
+///
+/// ***************************************************************************
+/// File last update : 2025-05-10T18:56:48.000+02:00
+/// Signature : c6fb8f0a9f961fb342c92d458fcacbbf8c79a248
+/// ***************************************************************************
+/// </summary>
+
 unit uSceneGameOver;
 
 interface
+
+// TODO : prendre en charge le changement de bouton "close" de la boite de dialogue ancêtre
 
 uses
   System.SysUtils,
@@ -14,12 +48,24 @@ uses
   FMX.Forms,
   FMX.Dialogs,
   FMX.StdCtrls,
-  _ScenesAncestor;
+  cDialogBox,
+  FMX.Effects,
+  FMX.Layouts,
+  FMX.Controls.Presentation,
+  Olf.FMX.TextImageFrame,
+  FMX.Objects,
+  _ButtonsAncestor,
+  cImageButton;
 
 type
-  TGameOverScene = class(T__SceneAncestor)
+  TGameOverScene = class(TcadDialogBox)
+    FlowLayout1: TFlowLayout;
+    procedure btnOkClick(Sender: TObject);
   private
   public
+    procedure TranslateTexts(const Language: string); override;
+    procedure ShowScene; override;
+    procedure AddText(const Text: String);
   end;
 
 implementation
@@ -28,7 +74,67 @@ implementation
 
 uses
   uScene,
-  uConsts;
+  uConsts,
+  uBidiooGameData,
+  uBidiooScores,
+  udmAdobeStock_257148021;
+
+procedure TGameOverScene.AddText(const Text: String);
+var
+  txt: TOlfFMXTextImageFrame;
+begin
+  txt := TOlfFMXTextImageFrame.Create(self);
+  txt.Parent := FlowLayout1;
+  txt.height := 40;
+  txt.margins.Bottom := 10;
+  txt.Font := dmAdobeStock_257148021.ImageList;
+  txt.Text := Text;
+
+  TFlowLayoutBreak.Create(self).Parent := FlowLayout1;
+
+  if FlowLayout1.height < txt.position.y + txt.height + txt.margins.Bottom then
+    FlowLayout1.height := FlowLayout1.height + txt.height + txt.margins.Bottom;
+end;
+
+procedure TGameOverScene.btnOkClick(Sender: TObject);
+begin
+  TScene.Current := TSceneType.Home;
+end;
+
+procedure TGameOverScene.ShowScene;
+begin
+  inherited;
+
+  TBidiooScores.Current.Add(TBidiooGameData.Current.Score);
+end;
+
+procedure TGameOverScene.TranslateTexts(const Language: string);
+begin
+  inherited;
+
+  FlowLayout1.BeginUpdate;
+  try
+    // Empty the FlowLayout
+    while FlowLayout1.ChildrenCount > 0 do
+      FlowLayout1.Children[0].Free;
+    FlowLayout1.height := 0;
+  finally
+    FlowLayout1.EndUpdate;
+  end;
+
+  if (Language = 'fr') then
+  begin
+    txtTitle.Text := 'Game Over';
+    AddText('Votre score final:');
+    AddText(TBidiooGameData.Current.Score.ToString);
+  end
+  else
+  begin
+    txtTitle.Text := 'Game Over';
+    AddText('Your final score:');
+    AddText(TBidiooGameData.Current.Score.ToString);
+  end;
+end;
 
 initialization
 
