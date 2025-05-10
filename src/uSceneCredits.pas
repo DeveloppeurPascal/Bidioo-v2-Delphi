549cbf4c169996a511f56a4f240d15c66a63ac6a
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-10 19:25:58 +0200

    finished the credits scene

--- src/uSceneCredits.pas
+++ src/uSceneCredits.pas
@@ -1,3 +1,35 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Bidioo
+///
+/// Copyright 2013-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://bidioo.gamolf.fr
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Bidioo-v2-Delphi
+///
+/// ***************************************************************************
+/// File last update : 2025-05-10T18:55:44.000+02:00
+/// Signature : 824ac5c97e27a9f0b64d2538f4721499d2804b76
+/// ***************************************************************************
+/// </summary>
+
 unit uSceneCredits;
 
 interface
@@ -21,12 +53,14 @@ uses
   FMX.Layouts,
   FMX.Controls.Presentation,
   Olf.FMX.TextImageFrame,
-  FMX.Objects;
+  FMX.Objects,
+  _ButtonsAncestor,
+  cImageButton;
 
 type
   TCreditsScene = class(TcadDialogBox)
     Text1: TText;
-    procedure btnCloseClick(Sender: TObject);
+    procedure btnOkClick(Sender: TObject);
   private
   public
     procedure AfterConstruction; override;
@@ -52,7 +86,7 @@ begin
   Text1.TextSettings.Font.Size := Text1.TextSettings.Font.Size * 2;
 end;
 
-procedure TCreditsScene.btnCloseClick(Sender: TObject);
+procedure TCreditsScene.btnOkClick(Sender: TObject);
 begin
   TScene.Current := TSceneType.Home;
 end;
