0b57507cb023046e6e5ffcf96d6651b12fc85a00
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-10 19:24:45 +0200

    added a pressed button mode to images buttons

--- src/cImageButton.pas
+++ src/cImageButton.pas
@@ -1,3 +1,35 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Bidioo
+///
+/// Copyright 2013-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://bidioo.gamolf.fr
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Bidioo-v2-Delphi
+///
+/// ***************************************************************************
+/// File last update : 2025-05-10T16:53:28.000+02:00
+/// Signature : f4f4761635740a9748cd9ad1548c9455012b1225
+/// ***************************************************************************
+/// </summary>
+
 unit cImageButton;
 
 interface
@@ -16,7 +48,8 @@ uses
   FMX.StdCtrls,
   _ButtonsAncestor,
   FMX.Objects,
-  USVGIconesKolopach, FMX.Effects;
+  USVGIconesKolopach,
+  FMX.Effects;
 
 type
   TbtnImageButton = class(T__ButtonAncestor)
@@ -24,10 +57,20 @@ type
     ShadowEffect1: TShadowEffect;
   private
     FKind: TSVGIconesKolopachIndex;
+    FIsPressed: boolean;
+    FIsPressedButton: boolean;
     procedure SetKind(const Value: TSVGIconesKolopachIndex);
+    procedure SetIsPressed(const Value: boolean);
+    procedure SetIsPressedButton(const Value: boolean);
+
+  protected
+    procedure Click; override;
   public
-    constructor Create(AOwner: TComponent); override;
     property Kind: TSVGIconesKolopachIndex read FKind write SetKind;
+    property IsPressedButton: boolean read FIsPressedButton
+      write SetIsPressedButton;
+    property IsPressed: boolean read FIsPressed write SetIsPressed;
+    constructor Create(AOwner: TComponent); override;
     procedure Repaint; override;
   end;
 
@@ -38,20 +81,51 @@ implementation
 uses
   uSVGBitmapManager_InputPrompts;
 
+procedure TbtnImageButton.Click;
+begin
+  if IsPressedButton then
+    IsPressed := not IsPressed;
+  inherited;
+end;
+
 constructor TbtnImageButton.Create(AOwner: TComponent);
 begin
   inherited;
   FKind := TSVGIconesKolopachIndex.Coeur;
+  FIsPressed := false;
+  FIsPressedButton := false;
 end;
 
 procedure TbtnImageButton.Repaint;
 begin
-  if IsDown then
-    rImage.Opacity := 0.5
+  if IsPressedButton then
+  begin
+    if IsPressed then
+      rImage.Opacity := 0.5
+    else
+      rImage.Opacity := 1;
+  end
   else
-    rImage.Opacity := 1;
+  begin
+    if IsDown then
+      rImage.Opacity := 0.5
+    else
+      rImage.Opacity := 1;
+  end;
   rImage.Fill.Bitmap.Bitmap.Assign(getBitmapFromSVG(FKind, rImage.Width,
     rImage.Height, rImage.Fill.Bitmap.Bitmap.BitmapScale));
+end;
+
+procedure TbtnImageButton.SetIsPressed(const Value: boolean);
+begin
+  FIsPressed := Value;
+  Repaint;
+end;
+
+procedure TbtnImageButton.SetIsPressedButton(const Value: boolean);
+begin
+  FIsPressedButton := Value;
+  Repaint;
 end;
 
 procedure TbtnImageButton.SetKind(const Value: TSVGIconesKolopachIndex);
