765c951d9d93db7fe26d657273e19ff024743724
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-10 19:26:46 +0200

    finished the hall of fame scene

--- src/uSceneHallOfFame.pas
+++ src/uSceneHallOfFame.pas
@@ -1,6 +1,40 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Bidioo
+///
+/// Copyright 2013-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://bidioo.gamolf.fr
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Bidioo-v2-Delphi
+///
+/// ***************************************************************************
+/// File last update : 2025-05-10T18:56:18.000+02:00
+/// Signature : d5ef21f64c98cd99a99144dc39d63f7641f3014b
+/// ***************************************************************************
+/// </summary>
+
 unit uSceneHallOfFame;
 
 interface
+
+// TODO : prendre en charge le changement de bouton "close" de la boite de dialogue ancêtre
 
 uses
   System.SysUtils,
@@ -14,12 +48,24 @@ uses
   FMX.Forms,
   FMX.Dialogs,
   FMX.StdCtrls,
-  _ScenesAncestor;
+  cDialogBox,
+  FMX.Effects,
+  FMX.Layouts,
+  FMX.Controls.Presentation,
+  Olf.FMX.TextImageFrame,
+  FMX.Objects,
+  _ButtonsAncestor,
+  cImageButton;
 
 type
-  THallOfFameScene = class(T__SceneAncestor)
+  THallOfFameScene = class(TcadDialogBox)
+    FlowLayout1: TFlowLayout;
+    procedure btnOkClick(Sender: TObject);
   private
   public
+    procedure TranslateTexts(const Language: string); override;
+    procedure ShowScene; override;
+    procedure AddText(const Text: String);
   end;
 
 implementation
@@ -28,7 +74,81 @@ implementation
 
 uses
   uScene,
-  uConsts;
+  uConsts,
+  uBidiooScores,
+  udmAdobeStock_257148021;
+
+procedure THallOfFameScene.AddText(const Text: String);
+var
+  txt: TOlfFMXTextImageFrame;
+begin
+  txt := TOlfFMXTextImageFrame.Create(self);
+  txt.Parent := FlowLayout1;
+  txt.height := 40;
+  txt.margins.Bottom := 10;
+  txt.Font := dmAdobeStock_257148021.ImageList;
+  txt.Text := Text;
+
+  TFlowLayoutBreak.Create(self).Parent := FlowLayout1;
+
+  if FlowLayout1.height < txt.position.y + txt.height + txt.margins.Bottom then
+    FlowLayout1.height := txt.position.y + txt.height + txt.margins.Bottom;
+end;
+
+procedure THallOfFameScene.btnOkClick(Sender: TObject);
+begin
+  TScene.Current := TSceneType.Home;
+end;
+
+procedure THallOfFameScene.ShowScene;
+var
+  i, nb: integer;
+  PrecScore: int64;
+begin
+  inherited;
+
+  TBidiooScores.Current.SortByPointsDesc;
+
+  FlowLayout1.BeginUpdate;
+  try
+    // Empty the FlowLayout
+    while FlowLayout1.ChildrenCount > 0 do
+      FlowLayout1.Children[0].Free;
+    FlowLayout1.height := 0;
+  finally
+    FlowLayout1.EndUpdate;
+  end;
+
+  // Add scores to the FlowLayout
+  i := 0;
+  nb := 0;
+  PrecScore := 0;
+  while (i < TBidiooScores.Current.count) and (nb < 100) do
+  begin
+    if (PrecScore <> TBidiooScores.Current[i].Points) then
+    begin
+      PrecScore := TBidiooScores.Current[i].Points;
+      inc(nb);
+
+      AddText(TBidiooScores.Current[i].Points.ToString);
+    end;
+    inc(i);
+  end;
+end;
+
+procedure THallOfFameScene.TranslateTexts(const Language: string);
+begin
+  inherited;
+
+  if (Language = 'fr') then
+  begin
+    txttitle.Text := 'Meilleurs scores';
+  end
+  else
+  begin
+    txttitle.Text := 'Hall of fame';
+  end;
+end;
 
 initialization
 
