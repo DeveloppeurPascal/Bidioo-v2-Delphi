d02de62d79040ddc85e6ffeaae53defac4728917
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-10 19:27:51 +0200

    added an empty tutorial scene (wip)

---
+++ src/uSceneTutoriel.pas
@@ -0,0 +1,121 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Bidioo
+///
+/// Copyright 2013-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://bidioo.gamolf.fr
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Bidioo-v2-Delphi
+///
+/// ***************************************************************************
+/// File last update : 2025-05-10T18:58:56.000+02:00
+/// Signature : 9a086ab14b3e7bc4f57c0b60308afea34767a2e1
+/// ***************************************************************************
+/// </summary>
+
+unit uSceneTutoriel;
+
+interface
+
+// TODO : prendre en charge le changement de bouton "close" de la boite de dialogue ancêtre
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Graphics,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Dialogs,
+  FMX.StdCtrls,
+  cDialogBox,
+  FMX.Effects,
+  FMX.Layouts,
+  FMX.Controls.Presentation,
+  Olf.FMX.TextImageFrame,
+  FMX.Objects,
+  _ButtonsAncestor,
+  cImageButton;
+
+type
+  TTutorielScene = class(TcadDialogBox)
+    Text1: TText;
+    procedure btnOkClick(Sender: TObject);
+  private
+  public
+    procedure AfterConstruction; override;
+    procedure TranslateTexts(const Language: string); override;
+  end;
+
+implementation
+
+{$R *.fmx}
+
+uses
+  uScene,
+  uConsts,
+  uTxtAboutDescription,
+  uTxtAboutLicense;
+
+{ TCreditsScene }
+
+procedure TTutorielScene.AfterConstruction;
+begin
+  inherited;
+  Text1.TextSettings.Font.Size := Text1.TextSettings.Font.Size * 2;
+end;
+
+procedure TTutorielScene.btnOkClick(Sender: TObject);
+begin
+  TScene.Current := TSceneType.Home;
+end;
+
+procedure TTutorielScene.TranslateTexts(const Language: string);
+var
+  txtLicense: string;
+begin
+  inherited;
+
+  if (Language = 'fr') then
+  begin
+    txtLicense := 'Licence';
+    txttitle.Text := 'Credits';
+  end
+  else
+  begin
+    txtLicense := 'License';
+    txttitle.Text := 'Credits';
+  end;
+
+  Text1.Text := GetTxtAboutDescription(Language).trim + sLineBreak + sLineBreak
+    + '**********' + sLineBreak + '* ' + txtLicense + sLineBreak + sLineBreak +
+    GetTxtAboutLicense(Language).trim + sLineBreak + sLineBreak +
+    application.MainForm.Caption + ' ' + CAboutCopyright + sLineBreak;
+
+end;
+
+initialization
+
+TScene.RegisterScene<TTutorielScene>(TSceneType.Tutoriel);
+
+end.
