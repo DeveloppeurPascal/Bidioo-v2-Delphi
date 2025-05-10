609e1224faddbeefd839f3e79cb4c35736db8ade
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-10 19:25:26 +0200

    added the good logo for this game's about box

--- src/uDMAboutBoxLogoStorrage.pas
+++ src/uDMAboutBoxLogoStorrage.pas
@@ -1,9 +1,9 @@
 ﻿/// <summary>
 /// ***************************************************************************
 ///
-/// Gamolf FMX Game Starter Kit
+/// Bidioo
 ///
-/// Copyright 2024-2025 Patrick Prémartin under AGPL 3.0 license.
+/// Copyright 2013-2025 Patrick PREMARTIN under AGPL 3.0 license.
 ///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -15,28 +15,18 @@
 ///
 /// ***************************************************************************
 ///
-/// The "Gamolf FMX Game Starter Kit" is both a "technical" example of a video
-/// game developed in Delphi with everything you need inside and a reusable
-/// project template you can customize for your own games.
-///
-/// The files provided are fully functional. Numerous comments are included in
-/// the sources to explain how they work and what you need to copy, override
-/// or customize to make video games without starting from scratch.
-///
-/// ***************************************************************************
-///
 /// Author(s) :
 /// Patrick PREMARTIN
 ///
 /// Site :
-/// https://fmxgamestarterkit.developpeur-pascal.fr/
+/// https://bidioo.gamolf.fr
 ///
 /// Project site :
-/// https://github.com/DeveloppeurPascal/Gamolf-FMX-Game-Starter-Kit
+/// https://github.com/DeveloppeurPascal/Bidioo-v2-Delphi
 ///
 /// ***************************************************************************
-/// File last update : 2025-05-08T19:49:54.000+02:00
-/// Signature : 0f41f9b977c822b1268e01446c3023cbfc2db162
+/// File last update : 2025-05-10T14:13:26.000+02:00
+/// Signature : 6e33fee54536047f86e1ddce399e4de1e6014b31
 /// ***************************************************************************
 /// </summary>
 
@@ -44,10 +34,6 @@ unit uDMAboutBoxLogoStorrage;
 
 interface
 
-{$MESSAGE WARN 'Save uDMAboutBoxLogoStorrage.pas in your game folder to customize it in your game. Don''t change the template version if you want to be able to update it.'}
-// TODO : Save uDMAboutBoxLogoStorrage.pas in your game folder to customize it in your game. Don't change the template version if you want to be able to update it.
-
-// TODO : when you'll have a logo or icon for your game, don't forget to replace default one by yours in the TImageList
 uses
   System.SysUtils,
   System.Classes,
