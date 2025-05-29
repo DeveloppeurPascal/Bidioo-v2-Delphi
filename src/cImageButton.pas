0b2eba0c191c85339640baec7b041479f44e9785
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-29 20:09:42 +0200

    added an exposant to the image button, a sound effect, ...

--- src/cImageButton.pas
+++ src/cImageButton.pas
@@ -1,34 +1,34 @@
-﻿/// <summary>
-/// ***************************************************************************
-///
-/// Bidioo
-///
-/// Copyright 2013-2025 Patrick PREMARTIN under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-/// Patrick PREMARTIN
-///
-/// Site :
-/// https://bidioo.gamolf.fr
-///
-/// Project site :
-/// https://github.com/DeveloppeurPascal/Bidioo-v2-Delphi
-///
-/// ***************************************************************************
-/// File last update : 2025-05-10T16:53:28.000+02:00
-/// Signature : f4f4761635740a9748cd9ad1548c9455012b1225
-/// ***************************************************************************
-/// </summary>
+﻿(* C2PP
+  ***************************************************************************
+
+  Bidioo
+
+  Copyright 2013-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://bidioo.gamolf.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Bidioo-v2-Delphi
+
+  ***************************************************************************
+  File last update : 2025-05-29T19:30:54.000+02:00
+  Signature : b8bddca7b4f876de37cc89278b6ccc92b40c32a7
+  ***************************************************************************
+*)
 
 unit cImageButton;
 
@@ -49,29 +49,39 @@ uses
   _ButtonsAncestor,
   FMX.Objects,
   USVGIconesKolopach,
-  FMX.Effects;
+  FMX.Effects,
+  Olf.FMX.TextImageFrame;
 
 type
   TbtnImageButton = class(T__ButtonAncestor)
     rImage: TRectangle;
     ShadowEffect1: TShadowEffect;
+    rExposant: TRectangle;
+    txtExposant: TOlfFMXTextImageFrame;
   private
     FKind: TSVGIconesKolopachIndex;
     FIsPressed: boolean;
     FIsPressedButton: boolean;
+    FNumber: integer;
+    FShowNumber: boolean;
     procedure SetKind(const Value: TSVGIconesKolopachIndex);
     procedure SetIsPressed(const Value: boolean);
     procedure SetIsPressedButton(const Value: boolean);
+    procedure SetNumber(const Value: integer);
+    procedure SetShowNumber(const Value: boolean);
 
   protected
     procedure Click; override;
   public
+    property Number: integer read FNumber write SetNumber;
+    property ShowNumber: boolean read FShowNumber write SetShowNumber;
     property Kind: TSVGIconesKolopachIndex read FKind write SetKind;
     property IsPressedButton: boolean read FIsPressedButton
       write SetIsPressedButton;
     property IsPressed: boolean read FIsPressed write SetIsPressed;
     constructor Create(AOwner: TComponent); override;
     procedure Repaint; override;
+    procedure AfterConstruction; override;
   end;
 
 implementation
@@ -79,13 +89,32 @@ implementation
 {$R *.fmx}
 
 uses
-  uSVGBitmapManager_InputPrompts;
+  uSVGBitmapManager_InputPrompts,
+  udmAdobeStock_257147901,
+  uSoundEffects;
+
+procedure TbtnImageButton.AfterConstruction;
+begin
+  inherited;
+  txtExposant.BeginUpdate;
+  try
+    txtExposant.Font := dmAdobeStock_257147901.ImageList;
+    txtExposant.AutoSize := true;
+    txtExposant.Text := '';
+  finally
+    txtExposant.EndUpdate;
+  end;
+end;
 
 procedure TbtnImageButton.Click;
 begin
   if IsPressedButton then
     IsPressed := not IsPressed;
   inherited;
+  if IsPressedButton then
+    TSoundEffects.Current.Play(TSoundEffectType.ClicOption)
+  else
+    TSoundEffects.Current.Play(TSoundEffectType.Clic);
 end;
 
 constructor TbtnImageButton.Create(AOwner: TComponent);
@@ -94,6 +123,8 @@ begin
   FKind := TSVGIconesKolopachIndex.Coeur;
   FIsPressed := false;
   FIsPressedButton := false;
+  FNumber := 0;
+  FShowNumber := false;
 end;
 
 procedure TbtnImageButton.Repaint;
@@ -114,6 +145,13 @@ begin
   end;
   rImage.Fill.Bitmap.Bitmap.Assign(getBitmapFromSVG(FKind, rImage.Width,
     rImage.Height, rImage.Fill.Bitmap.Bitmap.BitmapScale));
+  if FShowNumber then
+  begin
+    rExposant.Visible := true;
+    txtExposant.Text := FNumber.ToString;
+  end
+  else
+    rExposant.Visible := false;
 end;
 
 procedure TbtnImageButton.SetIsPressed(const Value: boolean);
@@ -131,6 +169,18 @@ end;
 procedure TbtnImageButton.SetKind(const Value: TSVGIconesKolopachIndex);
 begin
   FKind := Value;
+  Repaint;
+end;
+
+procedure TbtnImageButton.SetNumber(const Value: integer);
+begin
+  FNumber := Value;
+  Repaint;
+end;
+
+procedure TbtnImageButton.SetShowNumber(const Value: boolean);
+begin
+  FShowNumber := Value;
   Repaint;
 end;
 
