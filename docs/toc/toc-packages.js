bb59dd768b697fb0e1eb9ececfff5463d6f678c8
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2026-06-19 18:57:53 +0200

    exported developer doc in the new DocInsight template

---
+++ docs/toc/toc-packages.js
@@ -0,0 +1,14 @@
+
+window.DocInsight = window.DocInsight || {};
+window.DocInsight.toc = window.DocInsight.toc || {};
+window.DocInsight.toc["toc-packages"] =   [
+      {
+        id: "index",
+        title: "Bidioo",
+          href: "index.html",
+      },      {
+        id: "Bidioo",
+        title: "Bidioo",
+          href: "Bidioo.html",
+      }  ]
+;
